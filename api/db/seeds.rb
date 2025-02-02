09784f79a8608d2da776eda351910aeac1bf94d4
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-02-02 17:21:53 -0300

    Seeds de clientes

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,14 +1,16 @@
-require 'csv'
+require 'faker'
 
-file_path = Rails.root.join('db', 'seeds', 'usuarios.csv')
+clientes = []
 
-CSV.foreach(file_path, headers: true) do |row|
-  Cliente.create!(
-    nome: row['nome'],
-    data_nascimento: row['data_nascimento'],
-    cpf: row['cpf'],
-    email: row['email']
-  )
+200.times do
+  clientes << {
+    nome: Faker::Name.name,
+    data_nascimento: Faker::Date.birthday(min_age: 18, max_age: 90),
+    cpf: Faker::CPF.numeric,
+    email: Faker::Internet.email
+  }
 end
 
-puts "Clientes importados com sucesso!"
+Cliente.create!(clientes)
+
+puts "Clientes gerados com sucesso!"
