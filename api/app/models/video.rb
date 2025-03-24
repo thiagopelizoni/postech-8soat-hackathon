47bc9f5327e00f50789a992d668dbed55cd3ff0e
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-03-24 11:14:56 -0300

    Métodos de listagens de Video de acordo com seu status

--- api/app/models/video.rb
+++ api/app/models/video.rb
@@ -4,11 +4,27 @@ class Video
 
   field :local_path, type: String
   field :remote_path, type: String
-  field :status, type: String, default: "recebido"  
+  field :status, type: String, default: "recebido"
   field :metadados, type: Hash
 
   belongs_to :cliente
 
   validates :local_path, presence: true
-  validates :status, inclusion: { in: %w[recebido processado finalizado] }
+  validates :status, inclusion: { in: %w[recebido armazenado processado finalizado] }
+
+  def self.recebido
+    where(status: 'recebido')
+  end
+
+  def self.armazenado
+    where(status: 'armazenado')
+  end
+
+  def self.processado
+    where(status: 'processado')
+  end
+
+  def self.finalizado
+    where(status: 'finalizado')
+  end
 end
