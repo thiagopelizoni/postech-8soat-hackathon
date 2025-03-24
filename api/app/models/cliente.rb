44fe8ccc96ad442bd3ba56ae449c79b884f0f001
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-03-24 11:16:58 -0300

    Ligação entre Cliente e Video

--- api/app/models/cliente.rb
+++ api/app/models/cliente.rb
@@ -11,6 +11,8 @@ class Cliente
   validates :nome, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  has_many :videos
+
   def self.authenticate(access_token)
     Cliente.find_by(access_token: access_token)
   end
