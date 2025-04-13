65c2eb6b1e8f00dd57fd21f8e987a97f6c12ff48
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-04-13 13:05:23 -0300

    Ajustes nas chaves do AWS SES

--- api/config/initializers/aws_ses.rb
+++ api/config/initializers/aws_ses.rb
@@ -2,7 +2,7 @@ require 'aws-sdk-rails'
 
 ActionMailer::Base.add_delivery_method :aws_sdk, Aws::Rails::Mailer,
   credentials: Aws::Credentials.new(
-    ENV.fetch('AWS_ACCESS_KEY_ID'),
-    ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    ENV.fetch('AWS_SES_ACCESS_KEY_ID'),
+    ENV.fetch('AWS_SES_SECRET_ACCESS_KEY')
   ),
   region: ENV.fetch('AWS_REGION')
